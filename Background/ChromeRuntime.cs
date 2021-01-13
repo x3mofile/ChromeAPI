8f485af5782fdbe831cd1fee21ff2afa29692c70
Jorge Picco <x3mofile@gmail.com>
2021-01-13 17:17:32 -0600

    Changed To StartListening

---
+++ Background/ChromeRuntime.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace x3.Chrome.Background
+{
+    public abstract class AbstractChromeRuntime:IChromeRuntime
+    {
+        public abstract void StartListeningToRuntimeMessages();
+        public event Action<CreateNewInactiveTabMessage> CreateNewInactiveTabMessage;
+
+        protected void StringMessageHandler(string stringMessage)
+        {
+            if(Chrome.CreateNewInactiveTabMessage.SharesTitle(stringMessage))
+            {
+                OnCreateNewInactiveTabMessage(Chrome.CreateNewInactiveTabMessage.FromMessage(stringMessage));
+            }
+        }
+
+        private void OnCreateNewInactiveTabMessage(CreateNewInactiveTabMessage createNewInactiveTabMessage)
+        {
+            CreateNewInactiveTabMessage?.Invoke(createNewInactiveTabMessage);
+        }
+
+    }
+
+    public interface IChromeRuntime
+    {
+        void StartListeningToRuntimeMessages();
+        event Action<CreateNewInactiveTabMessage> CreateNewInactiveTabMessage;
+    }
+    
+
+}
