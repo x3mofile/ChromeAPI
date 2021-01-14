7727d446c55e4357bda9517af8523c27157ef209
Jorge Picco <x3mofile@gmail.com>
2021-01-14 03:26:53 -0600

    Tiny wincy blanc space deleted

---
+++ Background/ChromeRuntimeMessagesHandler_Application.cs
@@ -0,0 +1,28 @@
+﻿
+
+namespace x3.Chrome.Background
+{
+    public class ChromeRuntimeMessagesHandler_Application:IChromeRuntimeMessagesHandler_Application
+    {
+        private readonly IChromeRuntimeMessagesHandler ChromeRuntimeMessagesHandler;
+
+        public ChromeRuntimeMessagesHandler_Application(IChromeRuntimeMessagesHandler chromeRuntimeMessagesHandler)
+        {
+            ChromeRuntimeMessagesHandler=chromeRuntimeMessagesHandler;
+        }
+        public void Run()
+        {
+            ChromeRuntimeMessagesHandler.StartHandlingRuntimeMessages();
+        }
+
+
+
+    }
+
+
+    public interface IChromeRuntimeMessagesHandler_Application
+    {
+        void Run();
+
+    }
+}
